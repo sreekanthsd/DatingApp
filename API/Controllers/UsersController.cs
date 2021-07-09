459d443b62c3c1a9402224e0bfa7056f54c6da30
Sreekanth.v <sreeskavettikatt@gmail.com>
2021-07-09 10:18:42 +0530

    Adding Debugger

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,9 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
